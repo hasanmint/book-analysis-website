1fe2c9bbeaaddd82da099e15209d3489e800dd68
hasanmint73 <hasanmint73@gmail.com>
2022-04-04 07:41:28 +0600

    created all type component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Welcome Product Analysis Website</h2>
+      <h2 className='text-3xl font-bold underline bg-sky-300'>Welcome Product Analysis Website</h2>
     </div>
   );
 }
