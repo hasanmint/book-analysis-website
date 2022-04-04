38c2d5ba4f81e5e1c7d796743f1c958bd9116077
hasanmint73 <hasanmint73@gmail.com>
2022-04-04 07:47:56 +0600

    React Router Intialize call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Reviews from './components/Reviews/Reviews';
+import Review from './components/Review/Review';
+import Dashboard from './components/Dashboard/Dashboard';
+import Blogs from './components/Blogs/Blogs';
+import About from './components/About/About';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <h2 className='text-3xl font-bold underline bg-sky-300'>Welcome Product Analysis Website</h2>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/review' element={<Review></Review>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
